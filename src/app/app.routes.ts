e1205fd885553574615c1e45ccb52518f9791a97
TRAORE Yasmine <afytra282@gmail.com>
2026-09-22 14:25:10 +0000

    Création du portfolio professionnel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Accueil } from './accueil/accueil';
+import { Apropos } from './apropos/apropos';
+import { Competences } from './competences/competences';
+import { Projets } from './projets/projets';
+import { Experience } from './experience/experience';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+
+  {
+    path: 'accueil',
+    component: Accueil
+  },
+
+  {
+    path: 'apropos',
+    component: Apropos
+  },
+
+  {
+    path: 'competences',
+    component: Competences
+  },
+
+  {
+    path: 'projets',
+    component: Projets
+  },
+
+  {
+    path: 'experience',
+    component: Experience
+  },
+
+  {
+    path: 'contact',
+    component: Contact
+  },
+
+  {
+    path: '',
+    redirectTo: 'accueil',
+    pathMatch: 'full'
+  }
+
+];
---
+++ src/app/competences/competences.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-competences',
+  styleUrl: './competences.css',
+  templateUrl: './competences.html',
+})
+export class Competences {}
